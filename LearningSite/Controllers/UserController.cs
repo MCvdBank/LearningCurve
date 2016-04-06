5c53aad33975800f093230fa2cf4d13c99dfdc5a
MCvdBank <mc.vanderbank@gmail.com>
2016-04-06 20:42:39 +0200

    Create new project.

---
+++ LearningSite/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LearningSite.Models;
+
+namespace LearningSite.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            User u1 = new User(1, "MC");
+
+
+            return View(u1);
+        }
+    }
+}
---
+++ LearningSite/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearningSite.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public User() { }
+
+        public User(int aID, string aName)
+        {
+            this.ID = aID;
+            this.Name = aName;
+        }
+    }
+}
