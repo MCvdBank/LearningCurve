be81dae14f0d06f0eac2a47f4fc1addf49fe9fd7
MCvdBank <mc.vanderbank@gmail.com>
2016-04-07 09:40:55 +0200

    Create project.
    Day 1 tutorial complete.

---
+++ LearnMVC/LearnMVC/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnMVC.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Hello MVC";
+        }
+
+        public ActionResult GetView()
+        {
+            return View("MyView");
+        }
+
+    }
+}
