90270c04de2d42ee9782c968032c1f86da8a7a6b
MCvdBank <mc.vanderbank@gmail.com>
2016-04-07 11:46:42 +0200

    Day 2 tutorial complete.

--- LearnMVC/LearnMVC/Controllers/TestController.cs
+++ LearnMVC/LearnMVC/Controllers/TestController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LearnMVC.Models;
+using LearnMVC.ViewModels;
 
 namespace LearnMVC.Controllers
 {
@@ -15,8 +17,55 @@ namespace LearnMVC.Controllers
 
         public ActionResult GetView()
         {
-            return View("MyView");
+            /// Use ViewModel
+            /*
+            Employee emp = new Employee();
+            emp.FirstName = "MC";
+            emp.LastName = "van der Bank";
+            emp.Salary = 20000;
+
+            EmployeeViewModel vmEmp = new EmployeeViewModel();
+            vmEmp.EmployeeName = emp.FirstName + " " + emp.LastName;
+            vmEmp.Salary = emp.Salary.ToString("C");
+            if (emp.Salary > 15000)
+            {
+                vmEmp.SalaryColor = "green";
+            }
+            else
+            {
+                vmEmp.SalaryColor = "yellow";
+            }
+            
+            return View("MyView", vmEmp);
+            */
+
+            /// User BusinessLayer
+            EmployeeListViewModel employeeListViewModel = new EmployeeListViewModel();
+            EmployeeBusinessLayer empBal = new EmployeeBusinessLayer();
+            List<Employee> employees = empBal.GetEmployees();
+            List<EmployeeViewModel> empViewModels = new List<EmployeeViewModel>();
+
+            foreach (Employee emp in employees)
+            {
+                EmployeeViewModel empViewModel = new EmployeeViewModel();
+                empViewModel.EmployeeName = emp.FirstName + " " + emp.LastName;
+                empViewModel.Salary = emp.Salary.ToString("C");
+                if (emp.Salary > 15000)
+                {
+                    empViewModel.SalaryColor = "green";
+                }
+                else
+                {
+                    empViewModel.SalaryColor = "yellow";
+                }
+                empViewModels.Add(empViewModel);
+            }
+            employeeListViewModel.Employees = empViewModels;
+            employeeListViewModel.UserName = "@dmin_Dud3";
+
+            return View("MyView", employeeListViewModel);
         }
+
 
     }
 }
---
+++ LearnMVC/LearnMVC/Models/EmployeeBusinessLayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearnMVC.Models
+{
+    public class EmployeeBusinessLayer
+    {
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Employee emp = new Employee();
+            emp.FirstName = "MC";
+            emp.LastName = "van der Bank";
+            emp.Salary = 10000;
+            employees.Add(emp);
+
+            Employee emp1 = new Employee();
+            emp1.FirstName = "RM";
+            emp1.LastName = "Lombaard";
+            emp1.Salary = 20000;
+            employees.Add(emp1);
+
+            return employees;
+        }
+    }
+}
